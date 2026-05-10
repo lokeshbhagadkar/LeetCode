fb655d06e43becb09bb9d6b9d57006e9f53f698f
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-05-10 15:38:10 +0530

    Time: 2 ms (65.12%), Space: 64.7 MB (37.27%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        //jo vector hme written karna hai wo hao ye
+        vector<int> ans;
+        int n = mat.size();
+        //oneCount -> will store max numbe of 1's in a row
+        int oneCount = INT_MIN;
+        //rowNo -> will store index of max no. of 1's wali row;
+        int rowNo = -101; //i can store -1 too
+
+        for(int i=0;i<n;i++){
+            //hrr row start hone se pahle initialize to 0
+            int count =0;
+            for(int j=0;j<mat[i].size();j++){
+                //if 1 found then increment count
+                if(mat[i][j] == 1){
+                    count++;
+                }
+                // /after row completion, compare count with oneCount
+                if(count > oneCount){
+                    oneCount = count;
+                    rowNo =i;
+                }
+            }
+
+        }
+        ans.push_back(rowNo);
+        ans.push_back(oneCount);
+        return ans;
+    }
+
+        
+    
+};
