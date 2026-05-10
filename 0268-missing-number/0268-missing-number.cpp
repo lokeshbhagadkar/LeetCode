2e4492dfda4c3fbe8415060fce98760941aaed95
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-05-10 15:08:04 +0530

    Time: 0 ms (100%), Space: 21.8 MB (69.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum =0;
         int n = nums.size();
+        //yaha pe hm pure array jo already given hai uss ak sum nikalenge
         for(int i=0;i<n;i++){
             sum = sum + nums[i];
         }
         
+        //aur yaha pe total sum me se usse subtract kareneg
         int totalSum = ((n)*(n+1))/2;
         int ans = totalSum - sum;
         return ans;
