7601f83161af2be989c9958fe395f64dc0360270
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-05-10 12:23:27 +0530

    Time: 3 ms (8.2%), Space: 30.6 MB (5.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int index=0;index<n;index++){
             int newIndex = (index + k) %n;
-            //hmm new array create krr ke uss me elements ad krr rahe hai
+            //hmm new array create krr ke uss me elements add krr rahe hai
             ans[newIndex] = nums[index];
         }
 
