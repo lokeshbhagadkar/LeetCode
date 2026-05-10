c2b6c176d1c60e49a5ed480ea41ece30faa79da8
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-05-10 16:08:32 +0530

    Time: 0 ms (100%), Space: 10.1 MB (67.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     //TC -. O(n^2)
+
+    //reverse ka manual fucntin
+    void reverseVector(vector<int>& arr){
+        int n = arr.size();
+        int start =0;
+        int end = n-1;
+
+        while(start < end){
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+
+    }
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         //transpose step :1 
@@ -15,8 +29,11 @@ public:
         //kitni row h --> 0 > (n-1)
         for(int i=0;i<n;i++){
             //hr row ko reverse karna h
-            reverse(matrix[i].begin(), matrix[i].end());
+                 // reverse(matrix[i].begin(), matrix[i].end());
+
+                
             //reverse ka code khud se dekh lo-> manual function 
+            reverseVector(matrix[i]);
         }
 
 
