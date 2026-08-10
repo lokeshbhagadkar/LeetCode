393591f636a37210314e70da91b21ec34e00ecc3
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-10 20:51:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.62%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     double myPow(double x, int n) {
         //declaring long long
+        //i is medium level question
         long long N =n;
         if(N<0){
             x =1/x;
