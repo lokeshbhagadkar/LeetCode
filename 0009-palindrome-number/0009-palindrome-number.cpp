680beeb1918bf2b2a333457d92921103f2214ad8
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-15 13:07:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.25%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        //yaha pe long int lene ka karan
+        //revNum = (revNum*10)+ digit; yaha pe overflow ho raha tha
         long int revNum=0;
         int dup = x;
+        //as mention in edgecase
         if(x<0){
             return false;
         }
@@ -12,6 +15,8 @@ public:
             x = x/10;
         }
         
+        //ye logic me dhyan dena
+        //jabb compare karege and it will return 0 or 1
         return dup==revNum;
     }
 };
