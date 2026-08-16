26eaa7399bf54af3d18c2c5947ac8450dd92eaf3
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-16 14:42:52 +0530

    Time: 8 ms (50.07%), Space: 7.7 MB (65.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int fib(int n) {
         //note for github--> this is just a recursive way
+
+        //....point to be noted//
+        //we have to do it with dynamic programming way
          if(n<=1) return n;
         return fib(n-1) + fib(n-2);
     }
