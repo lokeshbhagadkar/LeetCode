a8dbd48c3dbe32abe291d9d6b7480c34829d103e
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-17 14:40:07 +0530

    Time: 4 ms (17.9%), Space: 97.2 MB (87.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //this code is done on 17 aug
+        //also done to maintain streat /
+        //make sure 
         int minPrice = prices[0];
         int maxProfit =0;
 
