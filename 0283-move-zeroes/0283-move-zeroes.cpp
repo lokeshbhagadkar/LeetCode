458b94b4c9ff503e938e72e5a8478c8ddd80642c
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-18 17:57:06 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         //this is agin  solve to miantai streak
-        //this is agaon day 2 of
+        //this is agaon day 2 of maintainig streak
         int j =0;
         for(int i=0;i< nums.size();i++){
             if(nums[i] !=0){
