68ccc2d89faed62026e9fa9cae4342ffb2d95275
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-18 17:56:38 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        //this is agin 
         int j =0;
         for(int i=0;i< nums.size();i++){
             if(nums[i] !=0){
