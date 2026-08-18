0497add17ae3ba156088e9fa45d61ef9aa530f16
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-18 17:57:40 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         //this is agin  solve to miantai streak
         //this is agaon day 2 of maintainig streak
         //date 18 aug 26 
-        //understand question 
+        //understand question and resolve properly
         int j =0;
         for(int i=0;i< nums.size();i++){
             if(nums[i] !=0){
