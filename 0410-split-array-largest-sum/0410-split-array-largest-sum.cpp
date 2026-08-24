88be5dc44a5987729b0610b64de587c7efa44112
Lokesh Bhagadkar <bhag.loki@gmail.com>
2026-08-24 14:52:46 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.56%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        long long low = 0, high = 0;
+
+        // Minimum possible answer = largest element
+        // Maximum possible answer = total sum
+        for (int x : nums) {
+            low = max(low, (long long)x);
+            high += x;
+        }
+
+        while (low < high) {
+            long long mid = low + (high - low) / 2;
+
+            int parts = 1;
+            long long sum = 0;
+
+            for (int x : nums) {
+                if (sum + x <= mid) {
+                    sum += x;
+                } else {
+                    parts++;
+                    sum = x;
+                }
+            }
+
+            // Need more than k subarrays
+            // => mid is too small
+            if (parts > k) {
+                low = mid + 1;
+            }
+            else {
+                // mid is possible, try smaller
+                high = mid;
+            }
+        }
+
+        return (int)low;
+    }
+};
